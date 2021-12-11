c401f54e734874c2f0cbad1c716f8dc015d36bf7
Noyal Thomas <noyallthomas@gmail.com>
2021-12-11 16:57:33 +0530

    cwf

---
+++ src/components/Milestones.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Milestones extends Component {
+    render() {
+        return (
+            <div>
+                <h1>mile</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/Overview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Overview extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    over
+                </h1>
+            </div>
+        )
+    }
+}
